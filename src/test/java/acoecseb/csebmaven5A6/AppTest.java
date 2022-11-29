fb26dbfbbf323807e296dcd9e6d8e7813b1147ce
Lenovo <Lenovo@DESKTOP-2CUCI5T>
2022-11-29 15:54:45 +0530

    commit2

--- src/test/java/acoecseb/csebmaven5A6/AppTest.java
+++ src/test/java/acoecseb/csebmaven5A6/AppTest.java
@@ -1,9 +1,8 @@
 package acoecseb.csebmaven5A6;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
 /**
  * Unit test for simple App.
  */
@@ -12,15 +11,11 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+   @Test
     public void test() {
     	App a=new App();
     	int actual=a.add(15,15);
-    	int expected=30;
+    	int expected=35;
     	assertEquals(expected,actual);
     }
 }
